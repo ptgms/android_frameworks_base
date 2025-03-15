e2eae8afb02ff573954d199b3c0669926d1cd3a3
ptgms <me@ptgms.space>
2025-03-15 18:18:01 +0100

    Revert "attempt to remove the power button logic entirely, apart from wakeup"
    This reverts commit 9a1b974b650d5b6c1903db649b493d90d743234c.

--- services/tests/wmtests/src/com/android/server/policy/TestPhoneWindowManager.java
+++ services/tests/wmtests/src/com/android/server/policy/TestPhoneWindowManager.java
@@ -94,6 +94,7 @@ import com.android.dx.mockito.inline.extended.StaticMockitoSession;
 import com.android.internal.accessibility.AccessibilityShortcutController;
 import com.android.internal.policy.KeyInterceptionInfo;
 import com.android.internal.util.FrameworkStatsLog;
+import com.android.server.GestureLauncherService;
 import com.android.server.LocalServices;
 import com.android.server.input.InputManagerInternal;
 import com.android.server.input.KeyboardMetricsCollector.KeyboardLogEvent;
@@ -159,6 +160,7 @@ class TestPhoneWindowManager {
     @Mock private DisplayRotation mDisplayRotation;
     @Mock private DisplayPolicy mDisplayPolicy;
     @Mock private WindowManagerPolicy.ScreenOnListener mScreenOnListener;
+    @Mock private GestureLauncherService mGestureLauncherService;
     @Mock private GlobalActions mGlobalActions;
     @Mock private AccessibilityShortcutController mAccessibilityShortcutController;
 
@@ -279,6 +281,8 @@ class TestPhoneWindowManager {
                 () -> LocalServices.getService(eq(PowerManagerInternal.class)));
         doReturn(mDisplayManagerInternal).when(
                 () -> LocalServices.getService(eq(DisplayManagerInternal.class)));
+        doReturn(mGestureLauncherService).when(
+                () -> LocalServices.getService(eq(GestureLauncherService.class)));
         doReturn(mUserManagerInternal).when(
                 () -> LocalServices.getService(eq(UserManagerInternal.class)));
         doReturn(null).when(() -> LocalServices.getService(eq(VrManagerInternal.class)));
@@ -662,6 +666,13 @@ class TestPhoneWindowManager {
     void assertNoPowerSleep() {
         mTestLooper.dispatchAll();
         verify(mPowerManager, never()).goToSleep(anyLong(), anyInt(), anyInt());
+    }
+
+    void assertCameraLaunch() {
+        mTestLooper.dispatchAll();
+        // GestureLauncherService should receive interceptPowerKeyDown twice.
+        verify(mGestureLauncherService, times(2))
+                .interceptPowerKeyDown(any(), anyBoolean(), any());
     }
 
     void assertSearchManagerLaunchAssist() {
